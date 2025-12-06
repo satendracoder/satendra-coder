876b038139798e8eeb3eea93a0a2036fc0a40e6c
Satendra Rajput <satendracoder@gmail.com>
2025-12-06 12:03:22 +0530

    reupdate for deploy

--- src/app/admin/admin.routes.server.ts
+++ src/app/admin/admin.routes.server.ts
@@ -3,26 +3,26 @@ import { ServerRoute, RenderMode } from '@angular/ssr';
 export const ADMIN_SERVER_ROUTES: ServerRoute[] = [
   {
     path: 'admin/tutorials',
-    renderMode: RenderMode.Prerender, // SSR only (no prerender)
+    renderMode: RenderMode.Client, // SSR only (no prerender)
   },
   {
     path: 'admin/tutorials/create',
-    renderMode: RenderMode.Prerender, // SSR only (no prerender)
+    renderMode: RenderMode.Client, // SSR only (no prerender)
   },
   {
     path: 'admin/tutorials/edit/:id',
-    renderMode: RenderMode.Prerender, // SSR only (no prerender)
+    renderMode: RenderMode.Client, // SSR only (no prerender)
   },
   {
     path: 'admin/courses',
-    renderMode: RenderMode.Prerender, // SSR only (no prerender)
+    renderMode: RenderMode.Client, // SSR only (no prerender)
   },
   {
     path: 'admin/courses/create',
-    renderMode: RenderMode.Prerender, // SSR only (no prerender)
+    renderMode: RenderMode.Client, // SSR only (no prerender)
   },
   {
     path: 'admin/courses/edit/:id',
-    renderMode: RenderMode.Prerender, // SSR only (no prerender)
+    renderMode: RenderMode.Client, // SSR only (no prerender)
   },
 ];
